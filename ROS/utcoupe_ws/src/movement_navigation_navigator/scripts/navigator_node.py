c16aa74dff0142a42fb4dcf494ab59a294de5286
Mindstan <mindstan@hotmail.fr>
2017-10-17 19:20:56 +0200

    :sparkles: Added Goto service base

--- ROS/utcoupe_ws/src/movement_navigation_navigator/scripts/navigator_node.py
+++ ROS/utcoupe_ws/src/movement_navigation_navigator/scripts/navigator_node.py
@@ -8,5 +8,34 @@
 #----> si impossible retourne impossible à ai
 #--> retourner travail_fini à ai
 
+import rospy
+#from movement_navigation_navigator.msg import *
+from movement_navigation_navigator.srv import *
+
+def pointToStr(point):
+    return "(" + str(point.x) + "," + str(point.y) + ")"
+
+def handle_goto(req):
+    debugStr = "Asked to go from "
+    debugStr += pointToStr(req.posStart)
+    debugStr += " to " + pointToStr(req.posEnd)
+    rospy.logdebug(debugStr)
+    # sends the request to the pathfinder
+    # TODO
+    # then sends the path point per point to the arduino_asserv
+    # TODO
+    # then return success
+    return True
+
+def startNode():
+    rospy.init_node ('navigator')
+
+    s = rospy.Service ("/navigation/navigator/goto", Goto, handle_goto)
+    rospy.loginfo ("Ready to navigate!")
+    rospy.spin ()
+
 if __name__ == "__main__":
-    pass
+    try:
+        startNode()
+    except rospy.ROSInterruptException:
+        pass
