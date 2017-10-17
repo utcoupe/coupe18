8c731c1726de600d11740fea876c44e7e87629fc
Mindstan <mindstan@hotmail.fr>
2017-10-17 18:07:57 +0200

    :notebook_with_decorative_cover: Added new navigator package

---
+++ ROS/utcoupe_ws/src/movement_navigation_navigator/scripts/navigator_node.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*-coding:Utf-8 -*
+
+# reçoit ordre de ai deplacement de posinit à posfin
+#--> demande chemin pathfinding
+#----> si impossible retourne impossible à ai
+#--> envoi chemin à arduino_asserv point par point
+#----> si impossible retourne impossible à ai
+#--> retourner travail_fini à ai
+
+if __name__ == "__main__":
+    pass
