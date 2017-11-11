6131de4ef30cddd5fdb00001653568c679d4fb46
Mindstan <mindstan@hotmail.fr>
2017-11-11 15:28:28 +0100

    :books: Added Documentation to the main node.

--- ROS/utcoupe_ws/src/movement_navigation_navigator/src/navigator_node.py
+++ ROS/utcoupe_ws/src/movement_navigation_navigator/src/navigator_node.py
@@ -9,35 +9,70 @@ from movement_navigation_navigator.srv import Goto
 from Pathfinder import PathfinderClient
 from Asserv import AsservClient
 
-WAITING = 0
+__author__ = "Gaëtan Blond"
+__date__ = 17/10/2017
+
+# Constants used for the status of goto requests
+WAITING_FOR_RESULT = 0
 SUCCESS = 1
 FAILURE = 2
 
 def pointToStr(point):
+    """
+    Convert a geometry_msgs/Pose2D object (or any object with x an y properties) to a string
+    """
     return "(" + str(point.x) + "," + str(point.y) + ")"
 
 class NavigatorNode:
+    """
+    The NavigatorNode class is the link between the AI, the Pathfinder and the Asserv.
+    This node gets a movement order on ROS service. It can accept many at a time but is not design to.
+    The node will wait for the Pathfinder and the Asserv to advertize their services and actions before starting itself.
+    """
     def __init__ (self):
+        """
+        Initialize the node. Does not start it.
+        """
         self._pathfinderClient = ""
         self._asservClient = ""
         self._waitedResults = {}
     
     def _callbackForResults(self, idAct, result):
+        """
+        Callback called when a goto action to asserv ended.
+        It takes the id of the action and the result (DoGotoActionResult.result).
+        It will only uptade the status of the request.
+        @param idAct:   The id of the action.
+        @param result:  The result of the action.
+        """
         if result:
             self._waitedResults[idAct] = SUCCESS
         else:
             self._waitedResults[idAct] = FAILURE
     
     def _waitResult(self, idAct):
-        self._waitedResults[idAct] = WAITING
+        """
+        Wait for a goto (asserv) action to end.
+        It will raise an exception if the action ended but did not succeed.
+        @param idAct:   The id of the action.
+        """
+        self._waitedResults[idAct] = WAITING_FOR_RESULT
         rate = rospy.Rate(10)
-        while self._waitedResults[idAct] == WAITING:
+        while self._waitedResults[idAct] == WAITING_FOR_RESULT:
             rate.sleep()
         
         if self._waitedResults[idAct] == FAILURE:
             raise Exception("Path found but asserv can't reach a point!")
 
     def _handle_goto(self, req):
+        """
+        Callback for the Goto service from the node.
+        It will take the last position sent by the asserv as the start position.
+        It will contact the Pathfinder to get the path to follow, and then sent the path point by point to the asserv.
+        If something caused an abord (no path, can't move, ...) then it will return success=False.
+        Else it will return success=True
+        @param req:     Request containing the target position
+        """
         posStart = self._asservClient.currentPose
 
         debugStr = "Asked to go from "
@@ -64,6 +99,10 @@ class NavigatorNode:
             return False
 
     def _printPath (self, path):
+        """
+        Print the path in the debug log from ROS.
+        @param path:    An array of Pose2D
+        """
         debugStr = "Received path: ["
         for point in path:
             debugStr += pointToStr(point)
@@ -71,6 +110,9 @@ class NavigatorNode:
         rospy.logdebug (debugStr)
 
     def startNode(self):
+        """
+        Start the node and the clients.
+        """
         rospy.init_node ('navigator')
 
         self._pathfinderClient = PathfinderClient()
