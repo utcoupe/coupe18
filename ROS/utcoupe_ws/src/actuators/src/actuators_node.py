83f468725310fbaf8a9861523a96bd644714edd8
PPotiron <potarrot@gmail.com>
2017-11-18 10:32:17 +0100

    Setup actuators serveur action

--- ROS/utcoupe_ws/src/actuators/src/actuators_node.py
+++ ROS/utcoupe_ws/src/actuators/src/actuators_node.py
@@ -2,15 +2,26 @@
 # -*-coding:Utf-8 -*
 
 import rospy
+import actionlib
+import actuators.msg
 
 
 class ActuatorsNode():
     """Dispatch commands from AI to the correct node"""
 
-    def __init__(self):
-        self.node = rospy.init_node('actuators')
+    _result = actuators.msg.dispatchResult()
 
+    def __init__(self):
+        self._node = rospy.init_node('actuators')
+        self._action_name = 'dispatcher'
+        self._action_server = actionlib.SimpleActionServer(self._action_name, actuators.msg.dispatchAction, execute_cb=self.dispatch, auto_start = False)
+        self._action_server.start()
+
+    def dispatch(self, command):
+        rospy.loginfo('Command received !')
+        self._action_server.set_succeeded(True)
 
 if __name__ == '__main__':
     actuators_node = ActuatorsNode()
+    rospy.loginfo('Actuators node started')
     rospy.spin()
