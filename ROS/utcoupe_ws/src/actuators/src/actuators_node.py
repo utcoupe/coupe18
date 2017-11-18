144f4a9d5924b34f63b5420fc7ff4e0ef96df91d
PPotiron <potarrot@gmail.com>
2017-11-18 14:33:42 +0100

    Comit  PLS

--- ROS/utcoupe_ws/src/actuators/src/actuators_node.py
+++ ROS/utcoupe_ws/src/actuators/src/actuators_node.py
@@ -12,14 +12,25 @@ class ActuatorsNode():
     _result = actuators.msg.dispatchResult()
 
     def __init__(self):
-        self._node = rospy.init_node('actuators')
+        self._node = rospy.init_node('actuators', log_level=rospy.DEBUG)
         self._action_name = 'dispatcher'
         self._action_server = actionlib.SimpleActionServer(self._action_name, actuators.msg.dispatchAction, execute_cb=self.dispatch, auto_start = False)
+        self._arduino_client = actionlib.SimpleActionClient(
+            'arduino', actuators.msg.arduinoAction)
         self._action_server.start()
 
     def dispatch(self, command):
-        #rospy.loginfo('Command received !')
+        rospy.logdebug('Command received !')
+        if command.name != '':
+            
         self._action_server.set_succeeded(False)
+
+    def sendToArduino(self, ):
+        _arduino_client.wait_for_server()
+        goal = actuators.msg.arduinoGoal(order=20)
+        _arduino_client.send_goal(goal)
+        _arduino_client.wait_for_result()
+        _arduino_client.get_result()
 
 if __name__ == '__main__':
     actuators_node = ActuatorsNode()
--- ROS/utcoupe_ws/src/actuators/src/actuators_properties.py
+++ ROS/utcoupe_ws/src/actuators/src/actuators_properties.py
@@ -4,37 +4,31 @@
 import xml.etree.ElementTree as ET
 import os
 
-xml_path = os.path.dirname(__file__) + '/../definitions/actuators.xml'
-xml_root = ET.parse(xml_path).getroot()
-
+_xml_path = os.path.dirname(__file__) + '/../definitions/actuators.xml'
+_actuators_list = None
 #ActuatorsProperties
 class ActuatorsProperties():
     """Explore Actuators' Properties"""
-    def __init__(self,name=None,id=None):
-        for child in xml_root.iter('act'):
-            if child.get('id')==id or child.get('name')==name:
-                self.xml_actuators=child
-                break
-        self.id = self.xml_actuators.get('id')
-        self.name = self.xml_actuators.get('name')
-        self.type = self.xml_actuators.get('type')
-        self.family = self.xml_actuators.get('family')
-        self.default_timeout=self.xml_actuators.find('timeout').get('value')
-        positions = self.xml_actuators.find('positions')
+
+    def __init__(self, xml_actuators):
+        self.id = xml_actuators.get('id')
+        self.name = xml_actuators.get('name')
+        self.type = xml_actuators.get('type')
+        self.family = xml_actuators.get('family')
+        self.default_timeout=xml_actuators.find('timeout').get('value')
+        positions = xml_actuators.find('positions')
         self.default_position = positions.get('default')
         self.preset = {}
         for preset in positions.findall('preset'):
             self.preset[preset.get('name')] = preset.get('value')
-    
-# Test of the class
+
+def getActuatorsList():
+    if _actuators_list == None:
+        xml_root = ET.parse(xml_path).getroot()
+        _actuators_list = {}
+        for child in xml_root.iter('act'):
+            _actuators_list[child.get('name')] = ActuatorsProperties(child)
+    return _actuators_list
+
 if __name__ == '__main__':
-    xml = ActuatorsProperties(name='gripper_1');
-    print 'id: ' + xml.id
-    print 'name: ' + xml.name
-    print 'type: ' + xml.type
-    print 'family: ' + xml.family
-    print 'default_timeout: ' + xml.default_timeout
-    print 'default_position: ' + xml.default_position
-    print 'presets: '
-    for name, value in xml.preset.items():
-        print '\t' + name + ': ' + value
+    getActuatorsList()
