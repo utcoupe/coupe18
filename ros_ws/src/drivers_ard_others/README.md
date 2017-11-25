9b5ed70b5a589e95029099b9b32f8a5798846791
mouha <mohamet_ndiaye@yahoo.fr>
2017-11-25 22:00:12 +0100

    Modified for compatibility with belt_interpreter

---
+++ ros_ws/src/drivers_ard_others/README.md
@@ -0,0 +1,14 @@
+========== Installation ==========
+
+===== Rosserial =====
+
+sudo apt-get install ros-indigo-rosserial-arduino
+sudo apt-get install ros-indigo-rosserial
+
+========== Build ==========
+
+catkin_make drivers_ard_others_src_ard_others
+
+========= Upload ==========
+
+catkin_make drivers_ard_others_src_ard_others-upload
--- ros_ws/src/drivers_ard_others/src/ard_others_node.cpp
+++ ros_ws/src/drivers_ard_others/src/ard_others_node.cpp
@@ -1,7 +1,12 @@
+#define Address1  0x00
+#define Address2  0x00
+
 #include <ros.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Float32.h>
-#include <std_msgs/Empty.h>
+#include <std_msgs/Empty.h> 
+
+#include "processing_belt_interpreter/RangeList.h"
 
 #include <Arduino.h>
 
@@ -9,15 +14,17 @@ ros::NodeHandle  nh;
 
 std_msgs::Float32 value;
 std_msgs::String str_msg;
+processing_belt_interpreter::RangeList data_List;
 
 ros::Publisher chatter("chatter", &str_msg);
 
 // List of sensor publishers
-ros::Publisher pub_sensor1("sensor1", &value);
+ros::Publisher pub_sensor1("sensors/belt", &data_List);
 
 
 //Sensor addresses
-int sensorAddress = 0x00;
+int sensorAddress1 = Address1;
+int sensorAddress2 = Address2;
 
 //Sensor data variable
 int sensorPin = 11;
@@ -46,12 +53,19 @@ void loop()
   str_msg.data = hello;
   chatter.publish( &str_msg );
 
-  // Code to get sensors' data
+  // Get sensors' data
   sensorValue = analogRead(sensorPin);
 
   sensorValue = (sensorValue * 5 ) / 1023;
-  value.data = 0.123/sensorValue;
-  pub_sensor1.publish(&value);
+
+  // Publish sensors' data
+  data_List.sensors[1].sensor_id = "Capteur 1";
+  data_List.sensors[1].range = sensorValue;
+
+  data_List.sensors[2].sensor_id = "Capteur 2";
+  data_List.sensors[2].range = 1234;
+
+  pub_sensor1.publish(&data_List);
 
   nh.spinOnce();
   delay(500);
