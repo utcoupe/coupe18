4577457c0fa2d9d88cbdfa13f2566bbc4b1ed22d
Pierre Laclau <pielaclau@gmail.com>
2017-12-04 23:02:10 +0100

    Created universal game_status `status_services.py` class.

---
+++ ros_ws/src/ai_game_status/import/status_services.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import rospy
+
+from ai_game_status.srv import ArmRequest, NodeReady
+from ai_game_status.msg import GameEnd
+
+
+class StatusServices(object):
+    READY_SRV = "/ai/game_status/ready_notification"
+    HALT_SRV  = "/ai/game_status/status"
+
+    def __init__(self, namespace, packagename, arm_cb = None, halt_cb = None):
+        self._namespace, self._packagename = namespace, packagename
+        if arm_cb:
+            self._arm_srv   = rospy.Service("/{}/{}/arm".format(namespace, packagename), ArmRequest, arm_cb)
+        if halt_cb:
+            self._halt_srv  = rospy.Subscriber(self.HALT_SRV, GameEnd, halt_cb)
+
+    def ready(self, success):
+        rospy.wait_for_service(self.READY_SRV)
+        _ready_pub = rospy.ServiceProxy(self.READY_SRV, NodeReady)
+        _ready_pub("/{}/{}".format(self._namespace, self._packagename), success)
