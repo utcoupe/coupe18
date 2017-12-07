acfdb2e84d2ac1725eb2330301d86a2adad0e914
Thomas Fuhrmann <tomesman@gmail.com>
2017-12-07 19:20:04 +0100

    Because rosserial_arduino needs its own version of arduino, copy the arduino libraries we need in this version instead of using the old 1.0 one

--- scripts/install_utcoupe_setup.sh
+++ scripts/install_utcoupe_setup.sh
@@ -55,6 +55,12 @@ function install_ros() {
 		elif [ "$ARCH" = "armv7l" ]; then
 			sudo apt-get install ros-kinetic-ros-base ros-kinetic-tf2 ros-kinetic-tf2-ros ros-kinetic-rviz ros-kinetic-diagnostic-updater ros-kinetic-roslint ros-kinetic-camera-info-manager ros-kinetic-rosserial-arduino ros-kinetic-rosbridge-suite ros-kinetic-tf2-web-republisher
 		fi
+		# "Install" Arduino libs needed by us
+		if [ -d "/usr/share/arduino" ] && [ -d "$PWD/libs/arduino-libraries" ]; then
+			sudo cp -ar $PWD/libs/arduino-libraries/* /usr/share/arduino/libraries/
+		else
+			red_echo "Unable to locate arduino folder or arduino-libraries extracted folder, please check your setup"
+		fi
 		sudo rosdep init
 		rosdep update
 	else
@@ -106,10 +112,6 @@ function env_setup() {
 			tar -C $PWD/libs -xzf $f
 		fi
 	done
-	# "Install" Arduino libs
-	if [ ! -L "/opt/arduino-1.0" ]; then
-		sudo ln -s $PWD/libs/arduino-1.0 /opt/
-	fi
 	# Add the Ethernet IP address of raspberry pi to have a shortcut
 	if ! grep "utcoupe" /etc/hosts > /dev/null; then
 	    sudo sh -c "echo '#UTCoupe raspberry pi Ethernet IP when connected on the UTC network\n172.18.159.254	utcoupe_rpi31\n172.18.161.161	utcoupe_rpi32\n172.18.161.162	utcoupe_rpi33' >> /etc/hosts"
