df02225d8c879e52c027d1c77585f8235b0b962b
Pierre Laclau <pielaclau@gmail.com>
2017-12-10 01:23:27 +0100

    Finished MCQ interface.

--- arduino/common/hmi/hmi.ino
+++ arduino/common/hmi/hmi.ino
@@ -16,8 +16,9 @@ SSD1306  display(0x3c, D2, D1);
 OLEDDisplayUi ui(&display);
 
 //HMI variables
-String options[10] = ["Hii", "strategy_ftw"];
-uint8_t options_count = 2;
+String options[10];
+uint8_t options_count = 8;
+uint8_t cursor_pos;
 
 const char activeSymbol[] PROGMEM = {
     B00000000,
@@ -57,13 +58,35 @@ void drawBigCentralMessageComponent(OLEDDisplay *display, OLEDDisplayUiState* st
     display->drawString(64 + x, 22 + y, text);
 }
 
-void drawMCQComponent(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y, String options[], int num_options) {
-    int y_offset = 22;
-    display->setFont(ArialMT_Plain_16);
-    display->setTextAlignment(TEXT_ALIGN_CENTER);
+void drawMCQComponent(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
+    if(options_count > 0) {
+        int y_offset = 12;
+        display->setFont(ArialMT_Plain_10);
+        display->setTextAlignment(TEXT_ALIGN_LEFT);
+    
+        int start = cursor_pos - 1;
+        if(cursor_pos == 0) start = 0;
+        if(cursor_pos == options_count - 1) {
+          start = options_count - 3;
+          if(options_count == 2) start = options_count - 2;
+          if(options_count == 1) start = options_count - 1;
+        }
+    
+        for(int i = 0; i < 3; i++) {
+            display->drawString(11 + x, 16 + y + y_offset * i, options[start + i]);
+            if(cursor_pos == start + i) {
+                display->drawString(4 + x, 16 + y + y_offset * i, ">");
+            }
+        }
 
-    for(int i = 0; i < options_count; i++) {
-        display->drawString(14 + x, 22 + y, options[i]);
+        char b[5];
+        sprintf(b, "%d/%d",cursor_pos + 1,options_count);
+        display->drawString(4 + x, 52 + y, b);
+    }
+    else {
+        display->setFont(ArialMT_Plain_10);
+        display->setTextAlignment(TEXT_ALIGN_CENTER);
+        display->drawString(64 + x, 28 + y, "No options.");
     }
 }
 
@@ -74,10 +97,12 @@ void drawHelloFrame(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, 
 
 void drawTeamFrame(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
     drawFrameTitleComponent(display, state, x, y, "Team select");
+    drawMCQComponent(display, state, x, y);
 }
 
 void drawStrategyFrame(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
     drawFrameTitleComponent(display, state, x, y, "Strat select");
+    drawMCQComponent(display, state, x, y);
 }
 
 void drawArmFrame(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
@@ -128,6 +153,15 @@ void setup() {
 
     display.flipScreenVertically();
     ui.switchToFrame(5); //tmp for tests
+
+    options[0] = "strat1";
+    options[1] = "strategy_ftw";
+    options[2] = "rcva_going_to_death";
+    options[3] = "insa_is_trash";
+    options[4] = "hi";
+    options[5] = "MajusCules";
+    options[6] = "bonjour";
+    options[7] = "la derniere.";
 }
 
 
@@ -135,9 +169,17 @@ void loop() {
     int remainingTimeBudget = ui.update();
     if(Serial.available() > 0) { //tmp
           char c = Serial.read();
-          if(c == 'n') ui.nextFrame();
-          if(c == 'p') ui.previousFrame();
+          if(c == 'l') ui.nextFrame();
+          if(c == 'j') ui.previousFrame();
+          if(c == 'k' && options_count) {
+            cursor_pos += 1;
+            if(cursor_pos >= options_count) cursor_pos = options_count - 1;
+          }
+          if(c == 'i' && options_count) {
+            if(cursor_pos > 0) cursor_pos -= 1;
+          }
         }
+    Serial.println(cursor_pos);
     if (remainingTimeBudget > 0) {
         delay(remainingTimeBudget);
     }
