92ffa097521d9c339a0c6a52323e7e9e899e4dfa
Thomas Fuhrmann <tomesman@gmail.com>
2018-01-13 00:10:53 +0100

    Teraranger now returns -1 instead of -inf

--- ros_ws/src/drivers_ard_others/src/ard_others_teraranger.py
+++ ros_ws/src/drivers_ard_others/src/ard_others_teraranger.py
@@ -3,6 +3,7 @@
 import rospy
 from drivers_ard_others.msg import *
 from sensor_msgs.msg import Range
+from numpy import inf
 __author__ = "Thomas Fuhrmann"
 __date__ = 06/01/2018
 
@@ -21,6 +22,8 @@ class ArdOthersTeraranger:
             rospy.sleep(0.1)
 
     def _callback_teranranger_range(self, data):
+        if data.range == -inf:
+            data.range = -1
         self._range_value = float(data.range)
 
 
