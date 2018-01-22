bf2d15c6eaecb46ac83b750a09e57e9a0e42c0f8
milesial <milesial@users.noreply.github.com>
2018-01-22 13:43:57 +0100

    Basics of the action server

---
+++ ros_ws/src/drivers_ax12/src/angle_controller.py
@@ -0,0 +1,5 @@
+from dynamixel_controllers.joint_position_controller import JointPositionController
+
+
+class AngleController(JointPositionController):
+    pass
--- ros_ws/src/drivers_ax12/src/ax12_node.py
+++ ros_ws/src/drivers_ax12/src/ax12_node.py
@@ -14,7 +14,21 @@ class Ax12:
         self._act_goto = actionlib.ActionServer("ax12/command_action", AngleCommandAction,
                                                 self.action_callback, auto_start=False)
         self._act_goto.start()
+        rospy.spin()
 
     def action_callback(self, goal_handled):
         goal = goal_handled.get_goal()
+        self.add_goal(goal_handled)
 
+
+    def add_goal(self, goal_handled):
+        self.current_goals[self.next_goal_id] = goal_handled
+        self.next_goal_id += 1
+        goal_handled.set_accepted()
+
+    def remove_goal(self, goal_id):
+        self.current_goals.pop(goal_id)
+
+
+if __name__ == "__main__":
+    Ax12()
