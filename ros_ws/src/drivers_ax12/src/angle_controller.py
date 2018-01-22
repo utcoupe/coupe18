5d31b9f65a2861a4304d26375b02a3b5e9771f4a
milesial <milesial@users.noreply.github.com>
2018-01-22 15:51:37 +0100

    Simple launcher node for position control (not tested)

--- ros_ws/src/drivers_ax12/src/angle_controller.py
+++
@@ -1,5 +0,0 @@
-from dynamixel_controllers.joint_position_controller import JointPositionController
-
-
-class AngleController(JointPositionController):
-    pass
--- ros_ws/src/drivers_ax12/src/ax12_node.py
+++ ros_ws/src/drivers_ax12/src/ax12_node.py
@@ -3,18 +3,55 @@
 import rospy
 import actionlib
 from drivers_ax12.msg import *
+from drivers_port_finder.srv import GetPort
+import subprocess
 
 class Ax12:
+
     def __init__(self):
+        self.BAUD_RATE = 1000000
+        self.SCAN_RANGE = 10
+        self.PROFILE_VELOCITY = 200
+        self.PROFILE_ACCELERATION = 50
+
+        self.GET_PORT_SERVICE_NAME = "/drivers/port_finder/get_port"
+
         rospy.init_node('ax12', anonymous=False)
 
-        self.current_goals = {}
-        self.next_goal_id = 0
+        #self.current_goals = {}
+        #self.next_goal_id = 0
 
-        self._act_goto = actionlib.ActionServer("ax12/command_action", AngleCommandAction,
-                                                self.action_callback, auto_start=False)
-        self._act_goto.start()
-        rospy.spin()
+        #self._act_goto = actionlib.ActionServer("ax12/command_action", AngleCommandAction,
+        #                                        self.action_callback, auto_start=False)
+        #self._act_goto.start()
+
+        self.port = self.fetch_port()
+        self.init_params()
+        self.run_position_control_node()
+        rospy.loginfo("Position control node launched, ax12_node shutting down...")
+
+
+    def fetch_port(self):
+        try:
+            rospy.wait_for_service(self.GET_PORT_SERVICE_NAME, 10)
+            port = rospy.ServiceProxy(self.GET_PORT_SERVICE_NAME, GetPort)("usb2ax").port
+            if not port:
+                rospy.logerr("Error when fetching the port for usb2ax")
+            return port
+        except rospy.ROSException:
+            rospy.logerr("Port_finder service does not exist, can't fetch the ax12 port...")
+
+    def init_params(self):
+        rospy.set_param('device_name', self.port)
+        rospy.set_param('baud_rate', self.BAUD_RATE)
+        rospy.set_param('scan_range', self.SCAN_RANGE)
+        rospy.set_param('profile_velocity', self.PROFILE_VELOCITY)
+        rospy.set_param('profile_acceleration', self.PROFILE_ACCELERATION)
+
+    def run_position_control_node(self):
+        self.node_subprocess = subprocess.Popen(['rosrun', 'dynamixel_workbench_controllers', 'position_control',
+                                                 "__ns:=" + rospy.get_namespace()])
+
 
     def action_callback(self, goal_handled):
         goal = goal_handled.get_goal()
