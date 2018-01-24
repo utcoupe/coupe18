9102d2af1284e8f251c9eb1ba1180e068d224f5c
milesial <milesial@users.noreply.github.com>
2018-01-24 16:41:48 +0100

    Added wheel mode + no more def file and ros params

--- ros_ws/src/drivers_ax12/include/drivers_ax12/ax12_server.h
+++ ros_ws/src/drivers_ax12/include/drivers_ax12/ax12_server.h
@@ -15,15 +15,10 @@
 
 const double MAX_STOP_TIME = 5; //number of seconds to wait not moving before confirming the goal is not reached
 const double MAIN_FREQUENCY = 30;
-const std::string DEFINITIONS_SERVICE = "/memory/definitions/get";
-const std::string DEFINITION_PATH = "drivers/ax12.yaml";
 const std::string PORT_FINDER_SERVICE = "/drivers/port_finder/get_port";
 const std::string DEFAULT_PORT = "/dev/ttyACM0";
 const uint32_t BAUD_RATE = 10000000;
 const uint8_t SCAN_RANGE = 20;
-const uint16_t DEFAULT_VEL = 200; //1-1023 (if 0 then set to maximum) (0.111 rpm per unit)
-const uint16_t DEFAULT_MIN = 0; //0-1023
-const uint16_t DEFAULT_MAX = 1023; //0-1023
 
 
 typedef actionlib::ServerGoalHandle<drivers_ax12::AngleCommandAction> GoalHandle;
@@ -41,7 +36,7 @@ protected:
     uint8_t dxl_id_[SCAN_RANGE]; // ids of all the motors connected
     uint8_t dxl_cnt_; // number of ids in above array
 
-    std::list<GoalHandle> current_goals;
+    std::list<GoalHandle> joint_goals;
 
     // create messages that are used to published feedback/result
     drivers_ax12::AngleCommandFeedback feedback_;
@@ -51,14 +46,12 @@ protected:
 public:
     void execute_goal_cb(GoalHandle goal_handle);
     std::string fetch_port(const std::string& service_name);
-    std::string fetch_def_file_path(const std::string& service_name);
-    bool set_ros_params(const std::string& def_file_path);
     void init_workbench(const std::string& port);
-    bool init_motor(uint8_t motor_id);
     bool motor_id_exists(uint8_t motor_id);
     bool position_is_valid(uint8_t motor_id, uint16_t position);
     void main_loop(const ros::TimerEvent&);
-
+    bool handle_joint_goal(GoalHandle goal_handle);
+    bool handle_wheel_goal(GoalHandle goal_handle);
     Ax12Server(std::string name);
     ~Ax12Server();
 
--- ros_ws/src/drivers_ax12/src/ax12_server.cpp
+++ ros_ws/src/drivers_ax12/src/ax12_server.cpp
@@ -6,11 +6,8 @@ void Ax12Server::init_workbench(const std::string& port)
     /*
      * - initializes the DynamixelWorkbench
      * - scans the motors
-     * - filter the motors to keep only those configured in the ROS params
-     * - configure those (sets the init, min and max position)
      */
-    uint8_t nbr_of_ids;
-    uint8_t ids[SCAN_RANGE];
+
     std::string param_key;
 
     if(!dxl_wb_.begin(port.c_str(), BAUD_RATE)) {
@@ -19,96 +16,20 @@ void Ax12Server::init_workbench(const std::string& port)
         return;
     }
 
-    if(!dxl_wb_.scan(ids, &nbr_of_ids, SCAN_RANGE))
+    if(!dxl_wb_.scan(dxl_id_, &dxl_cnt_, SCAN_RANGE))
     {
         ROS_ERROR("Unable to scan the AX-12 motors on %s", port.c_str());
     }
 
-    ROS_INFO("Found %d AX-12 motors connected with a scan range of %d", nbr_of_ids, SCAN_RANGE);
+    ROS_INFO("Found %d AX-12 motors connected with a scan range of %d", dxl_cnt_, SCAN_RANGE);
 
-
-    for(uint8_t i = 0; i < nbr_of_ids; i++)
-    {
-        param_key = ros::names::append("motors", std::to_string(ids[i]));
-        param_key = ros::names::append(param_key, "init");
-
-        if(!ros::param::has(param_key))
-        {
-            ROS_WARN("No definition for AX-12 with ID %d, it will be ignored", ids[i]);
-        }
-        else
-        {
-            ROS_INFO("Definition for AX-12 with ID %d found, configuring...", ids[i]);
-            init_motor(ids[i]);
-            dxl_id_[dxl_cnt_++] = ids[i];
-
-        }
-    }
-}
-
-bool Ax12Server::init_motor(uint8_t motor_id)
-{
-    /*
-     * Sets the Present_Position, CW_Angle_Limit and CCW_Angle_Limit of a motor
-     * according to ros params. If params are not found, it will fall back to default
-     * constants
-     */
-
-    int param_value = 0;
-    bool write_success = true;
-
-    std::string base_key = ros::names::append("motors", std::to_string(motor_id));
-
-    write_success &= dxl_wb_.jointMode(motor_id, DEFAULT_VEL, 0);
-
-
-
-
-    // init pos
-    if(ros::param::get(ros::names::append(base_key, "init"), param_value))
-    {
-        write_success &= dxl_wb_.itemWrite(motor_id, "Present_Position", param_value);
-    }
-    else
-    {
-        ROS_ERROR("AX-12 with ID %d need a 'init' value in the definition file", motor_id);
-    }
-
-    // min pos
-    if(ros::param::get(ros::names::append(base_key, "min"), param_value))
-    {
-        write_success &= dxl_wb_.itemWrite(motor_id, "CW_Angle_Limit", param_value);
-    }
-    else
-    {
-        ROS_WARN("AX-12 with ID %d doesn't have a 'min' value in the definition file, falling back to default: %d", motor_id, DEFAULT_MIN);
-        write_success &= dxl_wb_.itemWrite(motor_id, "CW_Angle_Limit", DEFAULT_MIN);
-    }
-
-    // max pos
-    if(ros::param::get(ros::names::append(base_key, "max"), param_value))
-    {
-        write_success &= dxl_wb_.itemWrite(motor_id, "CCW_Angle_Limit", param_value);
-    }
-    else
-    {
-        ROS_ERROR("AX-12 with ID %d doesn't have a 'max' value in the definition file, falling back to default: %d", motor_id, DEFAULT_MAX);
-        write_success &= dxl_wb_.itemWrite(motor_id, "CCW_Angle_Limit", DEFAULT_MAX);
-    }
-
-    if(!write_success)
-    {
-        ROS_ERROR("Errors occured when configuring motor with id %d", motor_id);
-    }
-
-    return write_success;
 
 }
 
 bool Ax12Server::motor_id_exists(uint8_t motor_id)
 {
     /*
-     * Returns true if the motor was successfully detected and configured
+     * Returns true if the motor was successfully detected
      */
 
     for(uint8_t i = 0; i < dxl_cnt_; i++)
@@ -149,6 +70,8 @@ void Ax12Server::execute_goal_cb(GoalHandle goal_handle)
      */
 
     bool success = true;
+    auto goal = goal_handle.getGoal();
+    uint8_t motor_id = goal->motor_id;
 
     if(!goal_handle.isValid())
     {
@@ -157,8 +80,6 @@ void Ax12Server::execute_goal_cb(GoalHandle goal_handle)
         return;
     }
 
-    uint8_t motor_id = goal_handle.getGoal()->motor_id;
-
     if(!motor_id_exists(motor_id))
     {
         ROS_ERROR("AX-12 action server received a goal for motor ID %d, but no such motor was detected", motor_id);
@@ -166,31 +87,47 @@ void Ax12Server::execute_goal_cb(GoalHandle goal_handle)
         return;
     }
 
-    uint16_t position = goal_handle.getGoal()->position;
+    if(goal->mode == goal->JOINT)
+    {
+        handle_joint_goal(goal_handle);
+    }
+    else
+    {
+        handle_wheel_goal(goal_handle);
+    }
+}
+
+bool Ax12Server::handle_joint_goal(GoalHandle goal_handle)
+{
+    auto goal = goal_handle.getGoal();
+    uint8_t motor_id = goal->motor_id;
+    uint16_t position = goal->position;
+    bool success = true;
+
     if(!position_is_valid(motor_id, position))
     {
-        ROS_ERROR("AX-12 action server received a goal for motor ID %d, but with an invalid position: %d", motor_id, position);
+        ROS_ERROR("AX-12 action server received a joint goal for motor ID %d, but with an invalid position: %d", motor_id, position);
         goal_handle.setRejected();
-        return;
+        return false;
     }
 
-    uint16_t speed = goal_handle.getGoal()->speed;
+    uint16_t speed = goal->speed;
     if(speed < 0 || speed > 1023)
     {
-        ROS_ERROR("AX-12 action server received a goal for motor ID %d, but with an invalid speed: %d", motor_id, speed);
+        ROS_ERROR("AX-12 action server received a joint goal for motor ID %d, but with an invalid speed: %d", motor_id, speed);
         goal_handle.setRejected();
-        return;
+        return false;
     }
 
     goal_handle.setAccepted();
 
-    for(auto it = current_goals.begin(); it != current_goals.end();)
+    for(auto it = joint_goals.begin(); it != joint_goals.end();)
     {
         if(it->getGoal()->motor_id == motor_id)
         {
             it->setCanceled();
-            ROS_INFO("AX-12 action server received a goal for motor ID %d while another goal was running for that motor. The old goal was canceled.", motor_id);
-            it = current_goals.erase(it);
+            ROS_INFO("AX-12 action server received a joint goal for motor ID %d while another joint goal was running for that motor. The old goal was canceled.", motor_id);
+            it = joint_goals.erase(it);
         }
         else
         {
@@ -198,7 +135,7 @@ void Ax12Server::execute_goal_cb(GoalHandle goal_handle)
         }
     }
 
-    success &= dxl_wb_.goalSpeed(motor_id, speed);
+    success &= dxl_wb_.jointMode(motor_id, speed, 50);
     success &= dxl_wb_.goalPosition(motor_id, position);
 
     if(!success) {
@@ -206,8 +143,52 @@ void Ax12Server::execute_goal_cb(GoalHandle goal_handle)
     }
     else
     {
-        current_goals.push_back(goal_handle);
+        joint_goals.push_back(goal_handle);
     }
+
+    return success;
+}
+
+bool Ax12Server::handle_wheel_goal(GoalHandle goal_handle)
+{
+    auto goal = goal_handle.getGoal();
+    uint8_t motor_id = goal->motor_id;
+    uint16_t speed = goal->speed;
+    bool success;
+
+    if(speed < 0 || speed > 2047)
+    {
+        ROS_ERROR("AX-12 action server received a wheel goal for motor ID %d, but with an invalid speed: %d", motor_id, speed);
+        goal_handle.setRejected();
+        return false;
+    }
+
+    goal_handle.setAccepted();
+
+    for(auto it = joint_goals.begin(); it != joint_goals.end();)
+    {
+        if(it->getGoal()->motor_id == motor_id)
+        {
+            it->setCanceled();
+            ROS_INFO("AX-12 action server received a wheel goal for motor ID %d while another joint goal was running for that motor. The old goal was canceled.", motor_id);
+            it = joint_goals.erase(it);
+        }
+        else
+        {
+            it++;
+        }
+    }
+
+    success = dxl_wb_.wheelMode(motor_id, speed, 50);
+
+    if(!success) {
+        goal_handle.setAborted();
+    } else
+    {
+        goal_handle.setSucceeded();
+    }
+
+    return success;
 
 }
 
@@ -223,7 +204,7 @@ void Ax12Server::main_loop(const ros::TimerEvent&)
     int32_t goal_position;
     ros::Rate r(MAIN_FREQUENCY);
 
-    for(auto it = current_goals.begin(); it != current_goals.end();)
+    for(auto it = joint_goals.begin(); it != joint_goals.end();)
     {
         motor_id = it->getGoal()->motor_id;
         curr_position = dxl_wb_.itemRead(motor_id, "Present_Position");
@@ -242,7 +223,7 @@ void Ax12Server::main_loop(const ros::TimerEvent&)
         {
             result_.success = true;
             it->setSucceeded();
-            it = current_goals.erase(it);
+            it = joint_goals.erase(it);
             ROS_INFO("AX-12 position goal succeeded for motor ID %d", motor_id);
 
         }
@@ -250,7 +231,7 @@ void Ax12Server::main_loop(const ros::TimerEvent&)
         {
             result_.success = false;
             it->setAborted();
-            it = current_goals.erase(it);
+            it = joint_goals.erase(it);
             ROS_ERROR("AX-12 position goal aborted for motor ID %d", motor_id);
         }
         else
@@ -303,80 +284,12 @@ std::string Ax12Server::fetch_port(const std::string& service_name)
     return port;
 }
 
-std::string Ax12Server::fetch_def_file_path(const std::string& service_name)
-{
-    /*
-     * Asks the definitions for the path of the definition file
-     */
-
-    std::string path;
-
-    if(!ros::service::waitForService(service_name, 10000))
-    {
-        ROS_ERROR("Failed to contact memory_definitions (service not up)");
-    }
-    else
-    {
-        ros::ServiceClient client = nh_.serviceClient<memory_definitions::GetDefinition>(service_name);
-        memory_definitions::GetDefinition srv;
-        srv.request.request = DEFINITION_PATH;
-
-        if (client.call(srv) && srv.response.success)
-        {
-            return srv.response.path;
-        }
-        else
-        {
-            ROS_ERROR("Failed to fetch the AX-12 definition file from memory_definitions");
-        }
-    }
-
-    if(path.length() == 0)
-    {
-        ROS_ERROR("The AX-12 definition file path is not set");
-    }
-
-    return path;
-
-}
-
-bool Ax12Server::set_ros_params(const std::string& def_file_path)
-{
-    /*
-     * Calls rosparam to set params from the definition file
-     */
-
-    std::string ns = ros::names::append(nh_.getNamespace(), "motors");
-
-    int needed = snprintf(nullptr, 0, "rosparam load %s %s",  def_file_path.c_str(), ns.c_str()) + 1;
-    if(needed < 0)
-    {
-        ROS_ERROR("Unable to load AX-12 parameters from definition file: can't build rosparam command");
-    }
-
-    char command[needed];
-
-    snprintf(command, (size_t)needed, "rosparam load %s %s", def_file_path.c_str(), ns.c_str());
-
-    int code = system(command);
-
-    if(code)
-    {
-        ROS_ERROR("Unable to load AX-12 parameters from definition file: rosparam failed");
-    }
-
-    return (code == 0);
-}
-
 Ax12Server::Ax12Server(std::string name) :
         as_(nh_, name, boost::bind(&Ax12Server::execute_goal_cb, this, _1), false)
 {
-    std::string def_file_path, port;
+    std::string port;
 
     as_.start();
-
-    def_file_path = fetch_def_file_path(DEFINITIONS_SERVICE);
-    set_ros_params(def_file_path);
 
     port = fetch_port(PORT_FINDER_SERVICE);
     init_workbench(port);
