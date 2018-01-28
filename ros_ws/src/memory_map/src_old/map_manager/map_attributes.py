0fc879e70645b5d53381962d37faf9cfff8009c2
Pierre Laclau <pielaclau@gmail.com>
2018-01-28 20:51:25 +0100

    YAML RAGEQUIT, going full XML.

---
+++ ros_ws/src/memory_map/src_old/map_manager/map_attributes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+from map_loader import LoadingHelpers
+from map_bases import DictManager
+
+
+class Position2D(DictManager):
+    def __init__(self, initdict):
+        LoadingHelpers.checkKeysExist(initdict, "frame_id", "x", "y", "type")
+        super(Position2D, self).__init__(initdict)
+
+
+class Shape2D(DictManager):
+    def __init__(self, initdict):
+        LoadingHelpers.checkKeysExist(initdict, "type")
+        # TODO : validate for each shape type
+        super(Shape2D, self).__init__(initdict)
+
+
+class MarkerRViz(DictManager):
+    def __init__(self, initdict):
+        LoadingHelpers.checkKeysExist(initdict, "ns", "id", "type", "scale", "z", "orientation", "color")
+        # TODO let some arguments be optional (orientation not given -> 0, 0, 0)
+        super(MarkerRViz, self).__init__(initdict)
+
+
+class Trajectory(): # TODO Inherit
+    def __init__(self, initdict):
+        pass
