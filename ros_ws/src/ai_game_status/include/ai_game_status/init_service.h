182477c6105bbdef7ea954880ec31dc9f95671d1
Mindstan <mindstan@hotmail.fr>
2018-02-15 10:20:07 +0100

    :wrench: C++ interface done, not tested yet

--- ros_ws/src/ai_game_status/include/ai_game_status/init_service.h
+++ ros_ws/src/ai_game_status/include/ai_game_status/init_service.h
@@ -1,21 +1,36 @@
 #ifndef AI_GAME_STATUS_INIT_SERVICE_H
 #define AI_GAME_STATUS_INIT_SERVICE_H
 
+#include <ros/ros.h>
+
 #include "ai_game_status/GameStatus.h"
 #include "ai_game_status/ArmRequest.h"
 #include "ai_game_status/NodeReady.h"
 
+#include <functional>
+
 class StatusServices
 {
 public:
-    StatusServices(const std::string& namespaceName, const std::string& packageName);
+    typedef std::function<bool()> ArmCallback_t;
+    typedef std::function<void(const ai_game_status::GameStatus::ConstPtr&)> StatusCallback_t;
+    
+    StatusServices(const std::string& namespaceName, const std::string& packageName, ArmCallback_t armCallback, StatusCallback_t statusCallback);
     
     void setReady(bool success);
 
 private:
+    enum class Errors {SERVICE_TIMEOUT, SERVICE_NOT_RESPONDING};
+    
     std::string _nodeName;
+    ArmCallback_t _armCallback;
+    StatusCallback_t _statusCallback;
+    ros::NodeHandle _nodeHandle;
+    ros::ServiceServer _armServer;
+    ros::Subscriber _gameStatusSubscriber;
 
     bool _on_arm(ai_game_status::ArmRequest::Request &req, ai_game_status::ArmRequest::Response &rep);
+    void _on_gameStatus(const ai_game_status::GameStatus::ConstPtr& msg);
 };
 
 #endif // AI_GAME_STATUS_INIT_SERVICE_H
--- ros_ws/src/ai_game_status/src/init_service.cpp
+++ ros_ws/src/ai_game_status/src/init_service.cpp
@@ -6,7 +6,48 @@ const string READY_SRV = "/ai/game_status/node_ready";
 const string ARM_SRV = "/arm";
 const string HALT_SRV = "/ai/game_status/status";
 
-StatusServices::StatusServices(const string& namespaceName, const string& packageName)
+StatusServices::StatusServices(const string& namespaceName, const string& packageName, ArmCallback_t armCallback, StatusCallback_t statusCallback) :
+    _armCallback(armCallback),
+    _statusCallback(statusCallback)
 {
     _nodeName = "/" + namespaceName + "/" + packageName;
+    if (_armCallback)
+        _armServer = _nodeHandle.advertiseService(ARM_SRV, &StatusServices::_on_arm, this);
+    if (_statusCallback)
+        _gameStatusSubscriber = _nodeHandle.subscribe(HALT_SRV, 10, &StatusServices::_on_gameStatus, this);
+}
+
+void StatusServices::setReady(bool success)
+{
+    try
+    {
+        if (!ros::service::waitForService(READY_SRV, ros::Duration(4.0)))
+            throw Errors::SERVICE_TIMEOUT;
+        ros::ServiceClient readyPub = _nodeHandle.serviceClient<ai_game_status::NodeReady>(READY_SRV);
+        ai_game_status::NodeReady msg;
+        msg.request.success = success;
+        msg.request.node_name = _nodeName;
+        if (!readyPub.call(msg))
+            throw Errors::SERVICE_NOT_RESPONDING;
+        if (success)
+            ROS_INFO_STREAM("Node " << _nodeName << " initialized successfully.");
+        else
+            ROS_ERROR_STREAM("Node " << _nodeName << " didn't initialize correctly.");
+    }
+    catch(...)
+    {
+        ROS_ERROR("status_services couldn't contact ai/game_status to send init notification.");
+    }
+}
+
+
+bool StatusServices::_on_arm(ai_game_status::ArmRequest::Request &req, ai_game_status::ArmRequest::Response &rep)
+{
+    rep.success = _armCallback();
+    return true;
+}
+
+void StatusServices::_on_gameStatus(const ai_game_status::GameStatus::ConstPtr& msg)
+{
+    _statusCallback(msg);
 }
