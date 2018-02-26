ee49c0c22781485837ca5ae86810cccbc8b03c40
Mindstan <mindstan@hotmail.fr>
2018-02-26 17:52:17 +0100

    :books: Added some documentation

--- ros_ws/src/ai_game_status/include/ai_game_status/init_service.h
+++ ros_ws/src/ai_game_status/include/ai_game_status/init_service.h
@@ -9,14 +9,38 @@
 
 #include <functional>
 
+/**
+ * @brief Main class of the lib `ai_game_status`.
+ * It implements most of the python script `init_service.py` does.
+ */
 class StatusServices
 {
 public:
+    /** @brief Type of the callback for the arm request */
     using ArmCallback_t = std::function<bool ()>;
+    /** @brief Type of the callback for the status event */
     using StatusCallback_t = std::function<void (const ai_game_status::GameStatus::ConstPtr &)>;
     
+    /**
+     * @brief Initialize the status service and saves the callbacks for the future requests and events.
+     * 
+     * Creates the service `(nodename)/arm` that will be called by `ai_game_status` main node when all the node in our network must arm.
+     * It subscribes also to the topic `/ai/game_status/status`. To avoid any compatibility problems between std C++ and boost, statusCallback is not directly connected, it will be called through _on_arm member function.
+     * 
+     * @param namespaceName The name of the node namespace.
+     * @param packageName The name of the package or node.
+     * @param armCallback Callback called when a resquest to arm the node has been received. The value returned by the callback is send as a response.
+     * @param statusCallback Callback called when there is an update on the status topic.
+     */
     StatusServices(const std::string& namespaceName, const std::string& packageName, ArmCallback_t armCallback = nullptr, StatusCallback_t statusCallback = nullptr);
     
+    /**
+     * @brief Notify the `ai/game_status` main node that the current node tried to launch and say if it succeeded.
+     * 
+     * Calls the service `/ai/game_status/node_ready` with the node name and the success boolean as parameters.
+     * 
+     * @param success Indicates if the node succeeded to start and initialize.
+     */
     void setReady(bool success);
 
 private:
