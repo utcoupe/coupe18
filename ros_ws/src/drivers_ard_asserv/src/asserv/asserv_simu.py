1289b21b0213b5f3c45ec8e3006d0fcbbbf1c0c0
milesial <milesial@users.noreply.github.com>
2018-04-16 19:38:54 +0200

    Asserv simu can now turn

--- ros_ws/src/drivers_ard_asserv/src/asserv/asserv_simu.py
+++ ros_ws/src/drivers_ard_asserv/src/asserv/asserv_simu.py
@@ -15,8 +15,15 @@ SEND_SPEED_RATE = 0.1  # in ms
 ASSERV_RATE = 0.05  # in ms
 # To be more accurate on position error, the speed has to be lower otherwise the goal reached check will fail
 ASSERV_ERROR_POSITION = 0.005  # in meters
+ASSERV_ERROR_ANGLE = 0.05  # in radians
 ASSERV_MINIMAL_SPEED = 0.05  # in m/s
 
+class AsservGoal():
+    def __init__(self, goal_id, pose, has_angle=False, direction=1):
+        self.goal_id = goal_id
+        self.pose = pose
+        self.has_angle = has_angle
+        self.direction = direction
 
 class AsservSimu(AsservAbstract):
     def __init__(self, asserv_node):
@@ -28,9 +35,11 @@ class AsservSimu(AsservAbstract):
         self._current_linear_speed = 0
         self._current_angular_speed = 0
         self._currently_moving = False
+        self._currently_rotating = False
         self._emergency_stop = False
         # First element of tuple is the goal_id (see ROS actionlib) and the second one is the goal position
-        self._current_goal = (-1, Pose2D(0, 0, 0))
+        # The last one is if the asserv should use the angle
+        self._current_goal = None
         # Distance between the robot and the goal when the current goal is set
         self._current_goal_initial_distance = 0
         # Angle between the robot and the goal when the current goal is set
@@ -43,7 +52,7 @@ class AsservSimu(AsservAbstract):
         # The speed is in m/s
         self._max_linear_speed = 0.5
         # The angular speed is in rad/s
-        self._max_angular_speed = 0.01
+        self._max_angular_speed = 1.0
         # ROS stuff
         self._tmr_pose_send = rospy.Timer(rospy.Duration(SEND_POSE_RATE), self._callback_timer_pose_send)
         self._tmr_speed_send = rospy.Timer(rospy.Duration(SEND_SPEED_RATE), self._callback_timer_speed_send)
@@ -61,7 +70,7 @@ class AsservSimu(AsservAbstract):
 
     def gotoa(self, goal_id, x, y, a, direction):
         rospy.loginfo("[ASSERV] Accepting goal (x = " + str(x) + ", y = " + str(y) + ", a = " + str(a) + ").")
-        self._start_trajectory(goal_id, x, y, a, direction)
+        self._start_trajectory(goal_id, x, y, a, direction, has_angle=True)
         return True
 
     def rot(self, goal_id, a, no_modulo):
@@ -92,8 +101,9 @@ class AsservSimu(AsservAbstract):
     def clean_goals(self):
         self._goals_list = []
         # TODO check to make it proper
-        self._current_goal = (-1, Pose2D(0, 0, 0))
+        self._current_goal = None
         self._currently_moving = False
+        self._currently_rotating = False
         return True
 
     def pause(self, pause):
@@ -124,30 +134,80 @@ class AsservSimu(AsservAbstract):
             self._current_pose = Pose2D(x, y, a)
         return return_value
 
+    def _is_pose_pos_in_margins(self, current_pose, goal_pose):
+        return (current_pose.x > goal_pose.x - ASSERV_ERROR_POSITION) and \
+               (current_pose.x < goal_pose.x + ASSERV_ERROR_POSITION) and \
+               (current_pose.y > goal_pose.y - ASSERV_ERROR_POSITION) and \
+               (current_pose.y < goal_pose.y + ASSERV_ERROR_POSITION)
+
+    def _is_angle_in_margins(self, current_theta, goal_theta):
+        return (current_theta % (2 * math.pi) + 2 * math.pi < goal_theta % (2 * math.pi) + 2 * math.pi + ASSERV_ERROR_ANGLE) and \
+               (current_theta % (2 * math.pi) + 2 * math.pi > goal_theta % (2 * math.pi) + 2 * math.pi - ASSERV_ERROR_ANGLE)
+
     def _callback_timer_asserv_computation(self, event):
         # First check if a goal has to be got from the list
-        if self._current_goal[0] == -1 and len(self._goals_list) > 0:
+        if self._current_goal is None and len(self._goals_list) > 0:
             self._current_goal = self._goals_list.pop(0)
-            rospy.loginfo("[ASSERV] Starting a new goal : x = " + str(self._current_goal[1].x) + " y = " + str(self._current_goal[1].y) + " a = " + str(self._current_goal[1].theta))
-            self._current_goal_initial_distance = ((self._current_goal[1].x - self._current_pose.x) ** 2 + (self._current_goal[1].y - self._current_pose.y) ** 2) ** 0.5
-            self._current_goal_initial_angle = math.atan2(self._current_goal[1].y - self._current_pose.y, self._current_goal[1].x - self._current_pose.x)
-            self._currently_moving = True
-        if self._currently_moving and not self._emergency_stop:
+            rospy.loginfo("[ASSERV] Starting a new goal : x = " + str(self._current_goal.pose.x) + " y = " + str(self._current_goal.pose.y) + " a = " + str(self._current_goal.pose.theta))
+            self._current_goal_initial_distance = ((self._current_goal.pose.x - self._current_pose.x) ** 2 + (self._current_goal.pose.y - self._current_pose.y) ** 2) ** 0.5
+            self._current_goal_initial_angle = math.atan2(self._current_goal.pose.y - self._current_pose.y, self._current_goal.pose.x - self._current_pose.x)
+
+
+
+            rospy.loginfo('initial angle : %f' % self._current_goal_initial_angle)
+            rospy.loginfo('current angle : %f' % self._current_pose.theta)
+            robot_wanted_angle = self._current_goal_initial_angle
+            robot_wanted_angle += math.pi if self._current_goal.direction == 0 else 0
+
+            if self._is_angle_in_margins(self._current_pose.theta, robot_wanted_angle):
+                self._currently_moving = True
+            else:
+                self._currently_rotating = True
+
+        if self._currently_rotating and not self._emergency_stop and self._current_goal is not None:
+            if self._is_pose_pos_in_margins(self._current_pose, self._current_goal.pose):
+                robot_wanted_angle = self._current_goal.pose.theta % (2*math.pi)
+            else:
+                robot_wanted_angle = math.atan2(self._current_goal.pose.y - self._current_pose.y, self._current_goal.pose.x - self._current_pose.x)
+                robot_wanted_angle += math.pi if self._current_goal.direction == 0 else 0
+
+            if self._is_angle_in_margins(self._current_pose.theta, robot_wanted_angle):
+                self._currently_rotating = False
+                self._currently_moving = True
+                self._current_angular_speed = 0
+                rospy.loginfo('reached angle')
+            else:
+
+                current_goal_angle = (robot_wanted_angle - self._current_pose.theta)
+
+                rospy.logerr('wanted %f, current %f' % (robot_wanted_angle, self._current_pose.theta))
+                if robot_wanted_angle + 2 * math.pi < self._current_pose.theta + 2 * math.pi:
+                    self._current_angular_speed = -self._max_angular_speed
+                else:
+                    self._current_angular_speed = self._max_angular_speed
+
+                rospy.loginfo('adding %f' % (self._current_angular_speed * ASSERV_RATE))
+
+                self._current_pose.theta += self._current_angular_speed * ASSERV_RATE
+
+                rospy.loginfo('current theta %f' % self._current_pose.theta)
+
+        if self._currently_moving and not self._emergency_stop and self._current_goal is not None:
             # Check if the robot is arrived
-            if ((self._current_pose.x > self._current_goal[1].x - ASSERV_ERROR_POSITION) and
-                    (self._current_pose.x < self._current_goal[1].x + ASSERV_ERROR_POSITION) and
-                    (self._current_pose.y > self._current_goal[1].y - ASSERV_ERROR_POSITION) and
-                    (self._current_pose.y < self._current_goal[1].y + ASSERV_ERROR_POSITION)):
+            if self._is_pose_pos_in_margins(self._current_pose, self._current_goal.pose):
                 rospy.loginfo("[ASSERV] Goal position has been reached !")
                 # TODO make it proper, set the angle without speed
-                if self._current_goal[1].theta != 0:
-                    self._current_pose.theta = self._current_goal[1].theta
-                self._node.goal_reached(self._current_goal[0], True)
-                self._current_goal = (-1, Pose2D(0, 0, 0))
+                if self._current_goal.has_angle and not self._is_angle_in_margins(self._current_pose.theta, self._current_goal.pose.theta):
+                    self._currently_rotating = True
+                    # self._current_pose.theta = self._current_goal.pose.theta
+                else:
+                    self._node.goal_reached(self._current_goal.goal_id, True)
+                    self._current_goal = None
+
                 self._currently_moving = False
                 self._current_linear_speed = 0
             else:
-                current_goal_distance = ((self._current_goal[1].x - self._current_pose.x) ** 2 + (self._current_goal[1].y - self._current_pose.y) ** 2) ** 0.5
+                current_goal_distance = ((self._current_goal.pose.x - self._current_pose.x) ** 2 + (self._current_goal.pose.y - self._current_pose.y) ** 2) ** 0.5
                 if current_goal_distance > self._current_goal_initial_distance / 2:
                     self._current_linear_speed += self._max_acceleration * ASSERV_RATE
                 else:
@@ -167,8 +227,9 @@ class AsservSimu(AsservAbstract):
     def _callback_timer_speed_send(self, event):
         self._node.send_robot_speed(RobotSpeed(0, 0, self._current_linear_speed, 0, 0))
 
-    def _start_trajectory(self, goal_id, x, y, a=0, direction=1): #direction=1 forward, 0 backward
+    def _start_trajectory(self, goal_id, x, y, a=0, direction=1, has_angle=False): #direction=1 forward, 0 backward
         # TODO use direction ?
-        self._current_pose.theta = math.atan2(y - self._current_pose.y, x - self._current_pose.x)
-        self._current_pose.theta += math.pi if direction == 0 else 0 # simulate a backward movement
-        self._goals_list.append((goal_id, Pose2D(x, y, a)))
+        # self._current_pose.theta = math.atan2(y - self._current_pose.y, x - self._current_pose.x)
+        # self._current_pose.theta += math.pi if direction == 0 else 0 # simulate a backward movement
+        self._goals_list.append(AsservGoal(goal_id, Pose2D(x, y, a), has_angle, direction))
+
