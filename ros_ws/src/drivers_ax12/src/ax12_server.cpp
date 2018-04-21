a164b3a2f2fc2ae11d81cb59445447f777a579d3
milesial <milesial@users.noreply.github.com>
2018-04-21 14:43:23 +0200

    Ax12 on arm is ok

--- ros_ws/src/drivers_ax12/src/ax12_server.cpp
+++ ros_ws/src/drivers_ax12/src/ax12_server.cpp
@@ -367,8 +367,9 @@ Ax12Server::Ax12Server(const std::string &action_name, const std::string &servic
     timer_ = nh_.createTimer(ros::Duration(1.0 / MAIN_FREQUENCY), &Ax12Server::main_loop, this);
 
     status_services_ = std::make_unique<StatusServices>(
-        "drivers", "ax12", [this, port](const ai_game_status::ArmRequest::ConstPtr &){
-            this->_on_armRequest(port);
+        "drivers", "ax12", [this](const ai_game_status::ArmRequest::ConstPtr &){
+            driver_.scan_motors();
+            driver_.toggle_torque(true);
         });
 
     status_services_->setReady(true);
@@ -377,11 +378,5 @@ Ax12Server::Ax12Server(const std::string &action_name, const std::string &servic
 Ax12Server::~Ax12Server() {
     driver_.toggle_torque(false);
     ros::shutdown();
-}
-
-void Ax12Server::_on_armRequest(std::string port)
-{
-    driver_.scan_motors();
-    driver_.toggle_torque(true);
 }
 
