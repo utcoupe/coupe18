11fbfb37c029bf41dd1cac35f2f1ff70f8130069
Thomas Fuhrmann <tomesman@gmail.com>
2018-04-26 19:36:53 +0200

    Make pins proper (not working yet, to be used with new version of HMI)

---
+++ arduino/common/hmi/pins.h
@@ -0,0 +1,23 @@
+//
+// Created by tfuhrman on 22/04/18.
+//
+
+#ifndef ARDUINO_PINS_H
+#define ARDUINO_PINS_H
+
+// LCD pins
+#define LCD_SDA D2 //can not use another one
+#define LCD_SCK D1 //can not use another one
+
+// Buttons matrix pins
+#define BTN_L1 D6
+#define BTN_L2 D1 //TODO to change
+#define BTN_R1 D5
+#define BTN_R2 D0
+
+// LED pins
+#define LED_RED     D3
+#define LED_BLUE    D4
+#define LED_GREEN   D2 //TODO to change
+
+#endif //ARDUINO_PINS_H
