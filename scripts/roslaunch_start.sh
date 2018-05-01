313645f47daaf4a97e007b99aab751002b261845
rpi3_1 <utcoupe@assos.utc.fr>
2018-05-01 21:39:58 +0200

    Service script now working (?)

--- scripts/roslaunch_start.sh
+++ scripts/roslaunch_start.sh
@@ -2,7 +2,7 @@
 # DO NOT DELETE : USED FOR INIT.D SERVICE
 source ~/.bashrc
 source /opt/ros/kinetic/setup.sh
-source ../ros_ws/devel/setup.sh
+source $UTCOUPE_WORKSPACE/ros_ws/devel/setup.sh
 
 /usr/bin/python /opt/ros/kinetic/bin/roslaunch memory_definitions general.launch robot:=$1 &
 sleep 2
