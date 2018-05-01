8c953e677df355f7b11a78cd46a637bfd929720a
milesial <milesial@users.noreply.github.com>
2018-05-01 16:56:44 +0200

    Generalized service script

--- scripts/roslaunch_start.sh
+++ scripts/roslaunch_start.sh
@@ -4,6 +4,6 @@ source /home/utcoupe/.bashrc
 source /opt/ros/kinetic/setup.sh
 source /home/utcoupe/coupe18/ros_ws/devel/setup.sh
 
-/usr/bin/python /opt/ros/kinetic/bin/roslaunch memory_definitions general.launch robot:=pr &
+/usr/bin/python /opt/ros/kinetic/bin/roslaunch memory_definitions general.launch robot:=$1 &
 sleep 2
 
