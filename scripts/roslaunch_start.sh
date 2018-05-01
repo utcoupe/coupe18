51acc4bc7311acd7319b99c0cfdc0c0a83eac24c
milesial <milesial@users.noreply.github.com>
2018-05-01 16:59:11 +0200

    Better service script

--- scripts/roslaunch_start.sh
+++ scripts/roslaunch_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # DO NOT DELETE : USED FOR INIT.D SERVICE
-source /home/utcoupe/.bashrc
+source ~/.bashrc
 source /opt/ros/kinetic/setup.sh
-source /home/utcoupe/coupe18/ros_ws/devel/setup.sh
+source ../ros_ws/devel/setup.sh
 
 /usr/bin/python /opt/ros/kinetic/bin/roslaunch memory_definitions general.launch robot:=$1 &
 sleep 2
