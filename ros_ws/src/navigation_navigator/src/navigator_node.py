bea27f649006c1a76e8387accc3522f4d246fc92
rpi3_1 <utcoupe@assos.utc.fr>
2018-05-09 13:47:21 +0200

    wtf with button now ?

--- ros_ws/src/navigation_navigator/src/navigator_node.py
+++ ros_ws/src/navigation_navigator/src/navigator_node.py
@@ -29,7 +29,7 @@ NODE_NAME       = "navigator"
 NODE_NAMESPACE  = "navigation"
 FULL_NODE_NAME  = "/" + NODE_NAMESPACE + "/" + NODE_NAME
 
-NB_MAX_TRY      = 3
+NB_MAX_TRY      = 7
 TIME_MAX_STOP   = 1 # sec
 
 # Constants used for the status of goto requests
